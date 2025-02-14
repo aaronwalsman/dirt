8fa09aa265e45849ad2b52303fdd60001b3f35c8
wcrisdog <144226765+wcrisdog@users.noreply.github.com>
2025-02-14 11:10:58 +0800

    slight alignment

--- dirt/gridworld2d/climate_pattern_year.py
+++ dirt/gridworld2d/climate_pattern_year.py
@@ -75,7 +75,7 @@ def simulate_full_climate(
         water = flow_step_twodir(terrain, water, flow_rate)
         
         # 2. Erosion
-        new_terrain = simulate_erosion_step(terrain, water, current_erosion, flow_rate, erosion_endurance, erosion_ratio)
+        new_terrain, current_erosion = simulate_erosion_step(terrain, water, current_erosion, flow_rate, erosion_endurance, erosion_ratio)
         current_erosion = reset_erosion_status(new_terrain, terrain, current_erosion)
 
         # 3. light
