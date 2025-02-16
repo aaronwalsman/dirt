e1adbed1980939240ff878de790ec3a1d2b8c4eb
wcrisdog <144226765+wcrisdog@users.noreply.github.com>
2025-02-16 21:11:01 +0800

    Delete dirt/gridworld2d/sound.py

--- dirt/gridworld2d/sound.py
+++
@@ -1,9 +0,0 @@
-import jax
-import jax.numpy as jnp
-
-"""
-Considering the realization of sound system, we could imitate what we have designed in smell system
-In the system, we have N sound producers, M sound receivers and C sound channels
-
-Here using "channels", we mean that people give different sound to express different meanings.
-"""
