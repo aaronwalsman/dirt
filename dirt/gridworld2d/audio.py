e9f827b39cb50f1e7585b074300f408c26717cbc
Vincent Wang <vincentwcr041219@gmail.com>
2025-02-22 20:23:07 +0800

    More Ideas added in audio

--- dirt/gridworld2d/audio.py
+++ dirt/gridworld2d/audio.py
@@ -4,19 +4,24 @@ def sample_audio(
     # TODO: Vincent explore ideas here
     
     '''
-    Idea 1: Starting from a static model with N^2 attenuation, then
+    Idea 1 (Convolution): Starting from a static model with N^2 attenuation, then
     use convolution kernal and newly-defined decay factors to update:
     S^(t+1) = \gamma*K*S^t + S_{audio source}^{t+1}
-    '''
 
-    '''
-    Idea 2: 
+    Idea 2 (Field): 
     Even more naively, we can simply apply advection, 
     where we push the sound intensity field across the grid according to a velocity field
     Apply the idea of ballistic propagation model
+
+
+    Add-on Idea:
+    -  We could try stopping the sound when the distance is large enough to save the diffusion
+    -  Using propagation delay for dynamic sound diffusion
+
     '''
     
 
+    
 
 
 
