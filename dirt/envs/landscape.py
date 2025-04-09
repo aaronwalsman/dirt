a830257c7866aefde6db2aa039669c9e8890f97a
Vincent Wang <vincentwcr041219@gmail.com>
2025-04-09 20:10:25 +0800

    Different level of Envs

--- dirt/envs/landscape.py
+++ dirt/envs/landscape.py
@@ -52,7 +52,7 @@ class LandscapeParams:
     # air
     wind_std : float = 0.1
     wind_reversion : float = 0.001
-    air_initial_temperature : float = 0.
+    air_initial_temperature : float = 25.
     air_initial_smell: float = 0.
 
     # light
@@ -262,7 +262,7 @@ def landscape(
             )
 
             # Temperature changed based on light and rain
-            new_air_temperature = temperature_step(
+            air_temperature = temperature_step(
                 day_length, 
                 day, 
                 water, 
@@ -280,7 +280,7 @@ def landscape(
             # Evaporate and rain based on temperature and air moisture
             water, air_moisture, rain_status = weather_step(
                 water, 
-                new_air_temperature, 
+                air_temperature, 
                 air_moisture, 
                 rain_status, 
                 params.evaporation_rate, 
@@ -294,7 +294,7 @@ def landscape(
                 erosion,
                 water,
                 wind_velocity,
-                new_air_temperature,
+                air_temperature,
                 air_moisture,
                 light_intensity,
                 air_smell,
@@ -303,41 +303,188 @@ def landscape(
                 day_status
             )
 
-            return new_state
-        
-
-            # # move water
-            # # - evaporate
-            # #   TODO: incorporate temperature?
-            # evaporation = jnp.minimum(
-            #     water, params.evaporation_rate * action.step_size)
-            # water = water - evaporation
-            # air_moisture = air_moisture + evaporation
-            
-            # # - rain
-            # #   TODO: incorporate temperature?
-            # rain = air_moisture >= params.rain_moisture_threshold
-            # water = water + jnp.where(rain, water + air_moisture, water)
-            # air_moisture = jnp.where(rain, 0, air_moisture)
-            
-            # - flow
-            #   TODO: iterate if water_flow_rate * step_size is too large
-
-            # terrain, water = water_erosion_step(
-            #     terrain, water, params.water_flow_rate * step_size)       
+            return new_state     
             
         
         step_functions.append(step)
     
     return init, *step_functions
 
+def landscape_naive(
+    params : TLandscapeParams = LandscapeParams(),
+    dtype : Any = DEFAULT_FLOAT_TYPE,
+):
+    
+    init_wind_velocity, step_wind_velocity = ou_process(
+        params.wind_std,
+        params.wind_reversion,
+        jnp.zeros((2,), dtype=dtype),
+    )
+    
+    def init(
+        key : chex.PRNGKey,
+    ) -> TLandscapeState :
+        
+        # terrain
+        # - use fractal_noise to generate an initial terrain grid
+        key, terrain_key = jrng.split(key)
+        terrain = fractal_noise(
+            terrain_key,
+            params.world_size,
+            params.terrain_octaves,
+            params.terrain_lacunarity,
+            params.terrain_persistence,
+            params.terrain_max_octaves,
+            params.terrain_unit_scale,
+            params.terrain_max_height,
+            dtype=dtype,
+        )
+        
+        # erosion
+        # - initialize erosion to be zero everywhere
+        erosion = jnp.zeros(params.world_size, dtype=dtype)
+        
+        # water
+        # - start with zero water everywhere
+        water = jnp.zeros(params.world_size, dtype=dtype)
+        
+        # - compute the total water we want distributed over the entire grid
+        total_water = (
+            params.water_per_cell * params.world_size[0] * params.world_size[1])
+        
+        # - then we will fill the lowest points on the grid with water until
+        #   the desired total water has been deposited in the grid
+        water_level = jnp.min(terrain)
+        
+        def water_level_cond(water_water_level):
+            water, water_level = water_water_level
+            return jnp.sum(water) < total_water
+        
+        def water_level_body(water_water_level):
+            water, water_level = water_water_level
+            water_level += params.water_initial_fill_rate # Suppose Fix
+            water = water_level - terrain
+            water = jnp.where(water < 0., 0., water)
+            water = water.astype(jnp.bfloat16)
+            return (water, water_level)
+        
+        water, water_level = jax.lax.while_loop(
+            water_level_cond, water_level_body, (water, water_level))
+        
+        # - scale the water everywhere in order to counteract any overshoot
+        current_water = jnp.sum(water)
+        water = water * (total_water / current_water)
+        
+        # air
+        key, wind_key = jrng.split(key)
+        wind_velocity = init_wind_velocity(wind_key)
+        air_temperature = jnp.full(
+            params.world_size, params.air_initial_temperature, dtype=dtype)
+        air_moisture = jnp.zeros(params.world_size, dtype=dtype)
+        air_light = jnp.zeros(params.world_size, dtype=dtype)
+        air_smell = jnp.zeros(params.world_size, dtype=dtype)
+        air_smell = air_smell[...,None]
+        rain_status = jnp.zeros(params.world_size, dtype=dtype)
+        day = params.day_initial
+        day_status = params.day_initial
+        
+        return LandscapeState(
+            terrain,
+            erosion,
+            water,
+            wind_velocity,
+            air_temperature,
+            air_moisture,
+            air_light,
+            air_smell,
+            rain_status,
+            day,
+            day_status
+        )
+    
+    step_functions = []
+    for step_size in params.step_sizes:
+        def step(
+            key : chex.PRNGKey,
+            action : TLandscapeAction,
+            state : TLandscapeState,
+        ) -> Tuple[TLandscapeState, TLandscapeObservation] :
+            
+            terrain = state.terrain
+            water = state.water
+            erosion = state.erosion
+            wind_velocity = state.wind_velocity
+            air_moisture = state.air_moisture
+            air_smell = state.air_smell
+            air_temperature = state.air_temperature
+            air_light = jnp.full(params.world_size, params.light_initial_strength, dtype=dtype)
+            day = state.day
+
+
+            rain_status = state.rain_status
+            day_length = params.steps_per_day
+            
+            # apply actions
+
+            # Day_status
+            day += 1
+            day_status = get_day_status(day_length, day_length / 2, day)
+            
+            # move air
+            # - update the wind direction
+            #   TODO: iterate if step_size is too large
+            key, wind_key = jrng.split(key)
+            wind_velocity = step_wind_velocity(
+                wind_key, wind_velocity, step_size=step_size)
+            
+            # - diffuse and move the air smell
+            diffusion_std = params.air_moisture_diffusion * (step_size**0.5)
+            air_smell = gas_step(
+                air_smell, diffusion_std, 1., wind_velocity, 1)
+            
+            # move water
+            water = flow_step(terrain, water, params.water_flow_rate)
+            # water = flow_step_twodir(terrain, water, params.water_flow_rate)
+
+            # erode based on water flow
+            old_terrain = terrain
+            terrain, erosion = simulate_erosion_step(
+                old_terrain, 
+                water, 
+                erosion, 
+                params.water_flow_rate, 
+                params.erosion_endurance, 
+                params.erosion_ratio
+            )
+            erosion = reset_erosion_status(terrain, old_terrain, erosion)
+
+            new_state = LandscapeState(
+                terrain,
+                erosion,
+                water,
+                wind_velocity,
+                air_temperature,
+                air_moisture,
+                air_light,
+                air_smell,
+                rain_status,
+                day,
+                day_status
+            )
+
+            return new_state    
+            
+        
+        step_functions.append(step)
+    
+    return init, *step_functions
 
 if __name__ == "__main__":
-    init, step_fn = landscape()
+    init, step_fn = landscape_naive()
     key = jax.random.PRNGKey(1234)
     state = init(key)
     action = LandscapeAction()
-    for i in range(2000):
+    for i in range(300):
         key, subkey = jax.random.split(key)
         state = step_fn(subkey, action, state)
         if i % 20 == 0:
