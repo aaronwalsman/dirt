3caef655bd68874946bd3786c7c037241d81f1d1
Aaron Walsman <aaronwalsman@gmail.com>
2025-06-11 12:53:28 -0400

    gas with clipping just at borders

--- dirt/gridworld2d/gas.py
+++ dirt/gridworld2d/gas.py
@@ -1,56 +1,121 @@
+import math
+
 import jax
 import jax.numpy as jnp
 import jax.random as jrng
 
 from dirt.constants import DEFAULT_FLOAT_DTYPE
 
-def box_kernel_std(kernel_radius):
-    return (
-        jnp.sqrt(kernel_radius * (kernel_radius+1)) /
-        jnp.sqrt(3.)
-    )
+from mechagogue.static import static_data, static_functions
 
-def gas(
+def make_gas(
     world_size,
+    downsample=1,
     cell_shape=(),
     initial_value=0.,
-    downsample=1,
     diffusion_std=0.333,
+    diffusion_radius=1,
     dissipation=0.,
     diffusion_type='box',
     boundary='clip',
     clip_fill=0.,
     include_diffusion=True,
     include_wind=True,
-    step_size=1.,
-    max_kernel_radius=3,
+    max_wind=16,
     float_dtype=DEFAULT_FLOAT_DTYPE,
 ):
     
+    assert world_size[0] % downsample == 0 and world_size[1] % downsample == 0
+    assert boundary in ('clip', 'roll', 'collect', 'redistribute')
     downsample_size = (world_size[0]//downsample, world_size[1]//downsample)
     grid_size = (*downsample_size, *cell_shape)
     
-    step_dissipation = dissipation ** (1./step_size)
+    max_wind_cells = int(math.ceil(max_wind / downsample))
+    #max_wind_cells = jnp.ceil(max_wind / downsample).astype(jnp.int32)
+    #xhi_edge = downsample_size[0]-max_wind_cells
+    #yhi_edge = downsample_size[1]-max_wind_cells
+    
+    if include_wind:
+        def wind_step(key, grid, wind):
+            wind = jnp.clip(wind, -max_wind, max_wind)
+            downsampled_wind = wind / downsample
+            wind_lo = jnp.floor(downsampled_wind).astype(jnp.int32)
+            wind_hi = jnp.ceil(downsampled_wind).astype(jnp.int32)
+            rounding = jrng.bernoulli(key, downsampled_wind-wind_lo)
+            discrete_wind = jnp.where(rounding, wind_hi, wind_lo)
+            
+            grid = jnp.roll(grid, shift=discrete_wind, axis=(0,1))
+            if boundary in ('clip', 'collect', 'redistribute'):
+                #xa = jnp.arange(downsample_size[0])
+                #x_mask = (
+                #    (xa >= discrete_wind[0]) &
+                #    (xa < downsample_size[0] + discrete_wind[0])
+                #)
+                #x_mask = x_mask[:,None,*((None,)*len(cell_shape))]
+                #ya = jnp.arange(downsample_size[1])
+                #y_mask = (
+                #    (ya >= discrete_wind[1]) &
+                #    (ya < downsample_size[1] + discrete_wind[1])
+                #)
+                #y_mask = y_mask[None,:,*((None,)*len(cell_shape))]
+                
+                xlo = jnp.arange(max_wind_cells)
+                xlo_mask = (
+                    (xlo >= discrete_wind[0]) &
+                    (xlo < downsample_size[0] + discrete_wind[0])
+                )
+                xlo_mask = xlo_mask[:,None,*((None,)*len(cell_shape))]
+                #xhi = jnp.arange(
+                #    xhi_edge, downsample_size[0])
+                xhi = xlo + downsample_size[0] - max_wind_cells
+                jax.debug.print('xhi: {x}', x=xhi)
+                xhi_mask = (
+                    (xhi >= discrete_wind[0]) &
+                    (xhi < downsample_size[0] + discrete_wind[0])
+                )
+                xhi_mask = xhi_mask[:,None,*((None,)*len(cell_shape))]
+                
+                ylo = jnp.arange(max_wind_cells)
+                ylo_mask = (
+                    (ylo >= discrete_wind[1]) &
+                    (ylo < downsample_size[1] + discrete_wind[1])
+                )
+                ylo_mask = ylo_mask[None,:,*((None,)*len(cell_shape))]
+                #yhi = jnp.arange(
+                #    yhi_edge, downsample_size[1])
+                yhi = ylo + downsample_size[1] - max_wind_cells
+                yhi_mask = (
+                    (yhi >= discrete_wind[1]) &
+                    (yhi < downsample_size[1] + discrete_wind[1])
+                )
+                yhi_mask = yhi_mask[None,:,*((None,)*len(cell_shape))]
+                
+                if boundary == 'clip':
+                    #grid = grid * x_mask
+                    grid = grid.at[:max_wind_cells].multiply(xlo_mask)
+                    grid = grid.at[-max_wind_cells:].multiply(xhi_mask)
+                    grid = grid.at[:,:max_wind_cells].multiply(ylo_mask)
+                    grid = grid.at[:,-max_wind_cells:].multiply(yhi_mask)
+                    
+                elif boundary == 'redistribute':
+                    edge_content = grid * ~x_mask
+                    edge_content = edge_content * ~y_mask
+                    redistribute_total = jnp.sum(edge_content, axis=(0,1))
+                
+                    grid = grid * x_mask
+                    grid = grid * y_mask
+                
+                    num_cells = (downsample_size[0]*downsample_size[1])
+                    grid = grid + redistribute_total / num_cells
+                
+                elif boundary == 'collect':
+                    pass
+            
+            return grid
     
     if include_diffusion:
-        step_std = diffusion_std * jnp.sqrt(step_size)
         
-        # do not use box if std is too low
-        min_box_std = box_kernel_std(1.)
-        if step_std < min_box_std:
-            diffusion_type = 'gaussian'
-        
-        # get the maximum std per iteration given the max_kernel_radius
-        if diffusion_type == 'gaussian':
-            max_iter_std = max_kernel_radius / 3.
-        elif diffusion_type == 'box':
-            max_iter_std = box_kernel_std(max_kernel_radius)
-        
-        # compute the number of iterations
-        iter_std = min(max_iter_std, step_std)
-        diffusion_iterations = jnp.ceil(
-            (step_std / iter_std)**2).astype(int)
-        
+        '''
         # make the kernel
         if diffusion_type == 'gaussian':
             radius = jnp.ceil(3 * iter_std).astype(int)
@@ -62,10 +127,14 @@ def gas(
                 (-1 + jnp.sqrt(1 + 12 * step_std ** 2)) / 2).astype(int)
             n = 2 * radius + 1
             kernel = jnp.ones(n, dtype=float_dtype) / n
+        '''
+        
+        n = 2 * diffusion_radius + 1
+        kernel = jnp.ones(n, dtype=float_dtype) / n
         
         kernel = kernel[:, None]
         kernel = kernel[..., None, None]
-        pad = radius * diffusion_iterations
+        pad = diffusion_radius #* diffusion_iterations
         
         def diffusion_step(grid):
             if len(grid.shape) == 2:
@@ -84,26 +153,26 @@ def gas(
             )
             
             channel_kernel = jnp.tile(kernel, (1, 1, 1, c))
-            for _ in range(diffusion_iterations):
-                # vertical
-                diffused_grid = jax.lax.conv_general_dilated(
-                    diffused_grid,
-                    channel_kernel,
-                    window_strides=(1,1),
-                    padding='VALID',
-                    dimension_numbers=('NHWC', 'HWIO', 'NHWC'),
-                    feature_group_count=c,
-                )
-                
-                # horizontal
-                diffused_grid = jax.lax.conv_general_dilated(
-                    diffused_grid,
-                    channel_kernel.transpose((1,0,2,3)),
-                    window_strides=(1,1),
-                    padding='VALID',
-                    dimension_numbers=('NHWC', 'HWIO', 'NHWC'),
-                    feature_group_count=c,
-                )
+            #for _ in range(diffusion_iterations):
+            # vertical
+            diffused_grid = jax.lax.conv_general_dilated(
+                diffused_grid,
+                channel_kernel,
+                window_strides=(1,1),
+                padding='VALID',
+                dimension_numbers=('NHWC', 'HWIO', 'NHWC'),
+                feature_group_count=c,
+            )
+            
+            # horizontal
+            diffused_grid = jax.lax.conv_general_dilated(
+                diffused_grid,
+                channel_kernel.transpose((1,0,2,3)),
+                window_strides=(1,1),
+                padding='VALID',
+                dimension_numbers=('NHWC', 'HWIO', 'NHWC'),
+                feature_group_count=c,
+            )
             diffused_grid = diffused_grid[0]
             
             if remove_last_channel:
@@ -111,52 +180,39 @@ def gas(
             
             return diffused_grid
     
-    if include_wind:
-        def wind_step(key, grid, wind):
-            grid = jnp.roll(grid, shift=wind, axis=(0,1))
-            #if boundary == 'clip':
-            #    # this won't even work (variable shaped tensors bro!)
-            #    h,w = grid.shape[:2]
-            #    wind_y, wind_x = wind
-            #    grid = grid.at[0:wind_y].set(clip_fill)
-            #    grid = grid.at[h+wind_y:h].set(clip_fill)
-            #    grid = grid.at[:,0:wind_x].set(clip_fill)
-            #    grid = grid.at[:,w+wind_x:w].set(clip_fill)
-            
+    @static_functions
+    class Gas:
+        
+        def init():
+            grid = jnp.zeros(grid_size, dtype=float_dtype)
+            grid = grid.at[:,:].set(initial_value)
+            return grid
+        
+        def step(key, grid, wind=None):
+            if dissipation:
+                grid = grid * (1. - dissipation)
+            if include_wind:
+                grid = wind_step(key, grid, wind)
+            if include_diffusion:
+                grid = diffusion_step(grid)
+            return grid
+        
+        def read(grid, x):
+            cell = x // downsample
+            cell_value = grid[cell[...,0], cell[...,1]]
+            return cell_value / downsample**2
+        
+        def write(grid, x, value):
+            cell = x // downsample
+            grid = grid.at[cell[...,0], cell[...,1]].set(value * downsample**2)
+            return grid
+        
+        def add(grid, x, value):
+            cell = x // downsample
+            grid = grid.at[cell[...,0], cell[...,1]].add(value)
             return grid
     
-    def init():
-        grid = jnp.zeros(grid_size, dtype=float_dtype)
-        grid = grid.at[:,:].set(initial_value)
-        return grid
-    
-    def step(key, grid, wind=None):
-        if step_dissipation:
-            grid = grid * (1. - step_dissipation)
-        if include_wind:
-            grid = wind_step(key, grid, wind)
-        if include_diffusion:
-            grid = diffusion_step(grid)
-        return grid
-    
-    def read(grid, x):
-        cell = x // downsample
-        c0, c1 = cell[...,0], cell[...,1]
-        return grid[c0, c1]
-    
-    def write(grid, x, value):
-        cell = x // downsample
-        c0, c1 = cell[...,0], cell[...,1]
-        grid = grid.at[c0, c1].set(value)
-        return grid
-    
-    def add(grid, x, value):
-        cell = x // downsample
-        c0, c1 = cell[...,0], cell[...,1]
-        grid = grid.at[c0, c1].add(value)
-        return grid
-    
-    return init, step, read, write, add
+    return Gas
 
 def step_old(
     gas_grid: jnp.ndarray,
