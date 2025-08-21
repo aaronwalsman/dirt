5719a1e01397c37d0ff02f16224fd8ef4f7ee893
Aaron Walsman <aaronwalsman@gmail.com>
2025-08-21 11:28:08 -0400

    removing print statements

--- dirt/visualization/viewer.py
+++ dirt/visualization/viewer.py
@@ -71,7 +71,6 @@ class Viewer:
         print_player_info=default_print_player_info,
     ):
         
-        print('A')
         self.get_report_block = standardize_args(
             get_report_block, ('report',))
         if get_active_players is not None:
@@ -119,7 +118,6 @@ class Viewer:
         
         self.world_size = world_size
         
-        print('B')
         self._init_params_and_reports(
             #example_params,
             #params_file,
@@ -128,7 +126,6 @@ class Viewer:
             step_0=step_0,
             start_step=start_step,
         )
-        print('C')
         self._init_context_and_window(window_size[0], window_size[1])
         self._init_splendor_render()
         self._init_landscape(terrain_texture_resolution)
